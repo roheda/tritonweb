31046484b98a662b1df087a8f1c81dd556fcffaa
roheda <rodrigoherreradavid@gmail.com>
2026-07-13 17:35:35 -0600

    Ampliar modelo de desarrollos con información comercial

--- app/Desarrollo.php
+++ app/Desarrollo.php
@@ -6,6 +6,42 @@ use Illuminate\Database\Eloquent\Model;
 
 class Desarrollo extends Model
 {
-    protected $fillable = array('id', 'idEstado', 'nombre', 'descripcion', 'brochure', 'video', 'imagen', 'logo', 'svg', 'slug', 'enlace', 'ubicacion', 'fecha', 'estatus');
-    protected $table    = 'desarrollos';
+    protected $fillable = array(
+        'id',
+        'idEstado',
+        'categoria',
+        'nombre',
+        'descripcion',
+        'descripcion_corta',
+        'tipo_desarrollo',
+        'tipo_operacion',
+        'tipo_producto',
+        'estado_comercial',
+        'precio_desde',
+        'precio_texto',
+        'mostrar_precio',
+        'etapa',
+        'brochure',
+        'video',
+        'imagen',
+        'logo',
+        'svg',
+        'slug',
+        'enlace',
+        'ubicacion',
+        'zona',
+        'ciudad',
+        'direccion',
+        'mapa_url',
+        'informacion_comercial',
+        'esquema_pago',
+        'disponibilidad_texto',
+        'meta_title',
+        'meta_description',
+        'imagen_social',
+        'fecha',
+        'estatus'
+    );
+
+    protected $table = 'desarrollos';
 }
