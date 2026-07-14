2e9a657400c0800f02ead3ea4abbd6a4487187b7
roheda <rodrigoherreradavid@gmail.com>
2026-07-14 09:23:12 -0600

    Reutiliza consultas de desarrollos y galerías durante la navegación

--- public/js/services/desarrolloservice.js
+++ public/js/services/desarrolloservice.js
@@ -3,19 +3,19 @@ angular.module('desarrolloService', [])
 	
 	return {
 		getDesarrollos: function() {
-			return $http.get('/getDesarrollos/0');
+			return $http.get('/getDesarrollos/0', { cache: true });
 		},
 		getUnidades: function(idDesarrollo) {
-			return $http.get('/getUnidades/' + idDesarrollo);
+			return $http.get('/getUnidades/' + idDesarrollo, { cache: true });
 		},
 		getDesarrollosYucatan: function() {
-			return $http.get('/getDesarrollos/31');
+			return $http.get('/getDesarrollos/31', { cache: true });
 		},
 		getDesarrollo: function(slug) {
-			return $http.get('/getDesarrollo/' + slug);
+			return $http.get('/getDesarrollo/' + slug, { cache: true });
 		},
 		getGaleria: function(param) {
-            return $http.get('/getGaleria/' + param);
-        },
-	}
+            return $http.get('/getGaleria/' + param, { cache: true });
+        }
+	};
 });
