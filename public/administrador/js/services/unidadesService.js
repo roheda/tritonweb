813ff2f2af49b30c62c1803655c7102bf73a6f3d
roheda <rodrigoherreradavid@gmail.com>
2026-07-13 18:46:37 -0600

    Actualizar servicio de unidades con estatus directo

--- public/administrador/js/services/unidadesService.js
+++ public/administrador/js/services/unidadesService.js
@@ -1,49 +1,48 @@
 angular.module('unidadesService', [])
 .factory('Unidad', function($http) {
-    
     return {
-        getUnidad : function(idDesarrollo){
-            return $http.get('dashboard/getUnidades/' + idDesarrollo);
+        getUnidad: function(idDesarrollo) {
+            return $http.get('/dashboard/getUnidades/' + idDesarrollo);
         },
-        getDesarrollos: function(){
-            return $http.get('dashboard/getActiveDesa/' + 0);
+        getDesarrollos: function() {
+            return $http.get('/dashboard/getActiveDesa/0');
         },
         saveUnidad: function(params) {
             return $http({
                 method: 'post',
-                headers: { 'Content-Type' : 'application/json' },
-                url: 'dashboard/newUnidad',
+                headers: { 'Content-Type': 'application/json' },
+                url: '/dashboard/newUnidad',
                 data: params
             });
         },
-        updateUnidad: function(id, params){
+        updateUnidad: function(id, params) {
             return $http({
                 method: 'post',
-                headers: { 'Content-Type' : 'application/json' },
-                url: 'dashboard/updateUnidad/' + id,
+                headers: { 'Content-Type': 'application/json' },
+                url: '/dashboard/updateUnidad/' + id,
                 data: params
             });
         },
-        directorioEstatus: function(id, params){
+        updateEstatus: function(id, estatus) {
             return $http({
                 method: 'post',
-                headers: { 'Content-Type' : 'application/json' },
-                url: 'dashboard/unidadEstatus/' + id,
-                data: params
+                headers: { 'Content-Type': 'application/json' },
+                url: '/dashboard/updateUnidadEstatus/' + id,
+                data: { estatus: estatus }
             });
         },
-        deleteUnidad : function(id){
+        deleteUnidad: function(id) {
             return $http({
                 method: 'post',
-                headers: { 'Content-Type' : 'application/json' },
-                url: 'dashboard/deleteUnidad/' + id
+                headers: { 'Content-Type': 'application/json' },
+                url: '/dashboard/deleteUnidad/' + id
             });
         },
         deleteFile: function(data) {
             return $http({
                 method: 'post',
-                headers: {'Content-Type': 'application/json'},
-                url: 'dashboard/deleteFile',
+                headers: { 'Content-Type': 'application/json' },
+                url: '/dashboard/deleteFile',
                 data: data
             });
         }
